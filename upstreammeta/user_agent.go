decd1273ce1ac939ad1cc403ba4618d0d98ba088
Fan Yang <missdeer@gmail.com>
2026-02-26 15:22:41 +0800

    update antigravity user agent

--- upstreammeta/user_agent.go
+++ upstreammeta/user_agent.go
@@ -4,5 +4,5 @@ const (
 	UserAgentCodexCLI      = "codex_cli_rs/0.101.0 (Mac OS 26.0.1; arm64) Apple_Terminal/464"
 	UserAgentGeminiCLI     = "google-api-nodejs-client/9.15.1"
 	UserAgentClaudeCodeCLI = "claude-cli/2.1.50 (external, sdk-cli)"
-	UserAgentAntigravity   = "antigravity/1.18.4 Darwin/arm64"
+	UserAgentAntigravity   = "antigravity/1.19.4 Darwin/arm64"
 )
